629c5dee7409466008501852f898ccaba58a5aea
rdonovan92 <rdonovan@illinois.edu>
2024-04-03 15:58:55 -0500

    Token work for New-TDXToken.ps1

--- src/UofITDXTickets/functions/public/New-TDXToken.ps1
+++ src/UofITDXTickets/functions/public/New-TDXToken.ps1
@@ -21,6 +21,25 @@ function New-TDXToken{
         if ($PSCmdlet.ShouldProcess("$($Script:Settings.BaseURI)session/")){
             $IVRSplat = @{
                 Headers = @{
+                'Content-Type' = 'application/json'
+                }
+                Method = 'POST'
+                URI = "$($Script:Settings.BaseURI)auth/"
+                }
+                $Body = @{
+                'username' = $Credential.UserName
+                'password' = $Credential.GetNetworkCredential().Password
+                } | ConvertTo-Json
+                $IVRsplat.add('Body', $Body)
+                $Token = Invoke-RestMethod @IVRSplat
+                if( $Token)}{
+                    $Script:Session = $Token
+                }
+                else{
+                    throw $_
+                }
+            # $IVRSplat = @{
+                Headers = @{
                     "X-Requested-With"="powershell"
                 }
                 Method = 'POST'
@@ -31,16 +50,15 @@ function New-TDXToken{
                     password = $Credential.GetNetworkCredential().Password
                 }
                 SessionVariable = 'Cookie'
-            }
-
-            Write-Verbose -Message "Generating Qualys API cookie for $($Credential.UserName)"
-            $Response = Invoke-RestMethod @IVRSplat
-            if( $Response.SIMPLE_RETURN.RESPONSE.TEXT -eq 'Logged in' ){
-                $Script:Session = $Cookie
-            }
-            else{
-                Throw $Response.SIMPLE_RETURN.RESPONSE.TEXT
-            }
+            # }
+            # Write-Verbose -Message "Generating Qualys API cookie for $($Credential.UserName)"
+            # $Response = Invoke-RestMethod @IVRSplat
+            # if( $Response.SIMPLE_RETURN.RESPONSE.TEXT -eq 'Logged in' ){
+                # $Script:Session = $Cookie
+            # }
+            # else{
+                # Throw $Response.SIMPLE_RETURN.RESPONSE.TEXT
+            # }
         }
     }
 }
