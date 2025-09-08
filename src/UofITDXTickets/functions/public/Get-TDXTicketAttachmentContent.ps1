e890e62c9fb5b04d3781dfe473061378c58d3de4
Tamara Buch <buch1@illinois.edu>
2025-09-08 16:32:48 -0500

    tested function, adding to repo

---
+++ src/UofITDXTickets/functions/public/Get-TDXTicketAttachmentContent.ps1
@@ -0,0 +1,34 @@
+<#
+.Synopsis
+    Gets the contents of an attachment.
+.DESCRIPTION
+    Gets the contents of an attachment.
+.PARAMETER AttachmentID
+    The ID of the Attachment. This can be found by inspecting the Attachments property returned by Get-TDXTicket.
+.EXAMPLE
+    Get-TDXTicketAttachmentContent -AttachmentID '888affd7-34f6-47d3-899e-79b2b45b59fd'
+#>
+function Get-TDXTicketAttachmentContent {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$AttachmentID,
+        [Parameter(Mandatory)]
+        [string]$OutputPath
+    )
+    process {
+
+        New-TDXSession -Credential $TDXCredential
+        $RestSplat = @{
+            Headers = @{
+                'Content-Type' = "application/json"
+                'Authorization' = "Bearer $($Script:Session)"
+            }
+            Method = 'GET'
+            URI = "https://help.uillinois.edu/TDWebApi/api/attachments/$($AttachmentID)/content"
+            OutFile = $OutputPath
+        }
+
+        $Response = (Invoke-RestMethod @RestSplat)
+        $Response
+    }
+}
